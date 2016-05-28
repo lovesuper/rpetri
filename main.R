4c6cfe739eccb4523b52c6488ccd8744b681ce86
lovesuper <antononrails@gmail.com>
2016-05-28 14:42:43 +0300

    next step in tha work

--- main.R
+++ main.R
@@ -1,46 +1,4 @@
-# Эксперимент с сетью Петри
-
-APlus = matrix(
-  c(
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 0, 0, 0,
-    0, 1, 0, 0, 0, 0, 1, 0, 0, 0,
-    0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 1
-    ), nrow=9, ncol=10, byrow=TRUE)
-
-AMinus = matrix(
-  c(
-    0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
-    0, 0, 1, 0, 0, 0, 0, 0, 1, 0,
-    0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
-    0, 0, 0, 0, 0, 0, 1, 0, 1, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0
-  ), nrow=9, ncol=10, byrow=TRUE)
-
-conflictedTransitions = matrix(
-  c(
-    0,
-    1,
-    1,
-    1,
-    0,
-    0,
-    0,
-    0,
-    0
-  )
-  , nrow=9, ncol=1)
-
-startingMarks = matrix(c( 1, 0, 1, 0, 1, 0, 1, 0, 0, 0), nrow=1, ncol=10)
+source("data.R")
 
 getAllowedTransitions <- function(currentState, inputFunc) {
   transposedInputFunc <- t(inputFunc)
@@ -71,7 +29,186 @@ getAllowedTransitions <- function(currentState, inputFunc) {
   return(allowedTransitions)
 }
 
-print(getAllowedTransitions(t(startingMarks), APlus))
+createStartingVector <- function(vector) {
+  vectorLength <- len(vector)
+  startingVector <- 0
+  q <- 1
+  for (i in 1:vectorLength) {
+    startingVector[i] <- q * vector[i]
+    if (vector[i] > 0) {
+      q <- 0
+    }
+  }
+   return(startingVector)
+}
+
+getTransactionNumber <- function(vector) {
+  vectorLength <- len(vector)
+  q <- 1
+  for (i in 1:vectorLength) {
+    if ((q * vector[i]) == 1) {
+      transitionNumber = i + 1
+    }
+    if (vector[i] > 0) {
+      q <- 0
+    }
+  }
+  return(transitionNumber)
+}
+
+getEventTimeByLambda <- function(lambda, time) {
+  randomTime <- (-1 / lambda) * ln(1 - rnd(1))
+  return(time + randomTime)
+}
+
+analysEvents <- function(lambda, time1, time2) {
+  randomTime <- (-1 / lambda) * ln(1 - rnd(1))
+  maxTime <- max(time1, time2)
+  return(maxTime + randomTime)
+}
+
+getSystemCharackteristics <- function(timeOnOutput, idleTime, time1, time2, deltaTime) {
+  timeOnOutput <- timeOnOutput + deltaTime
+  if (time1 >= time2) {
+    time <- time1 - time2
+  } else {
+    time <- 0
+  }
+  idleTime <- idleTime + time
+  return(c(timeOnOutput, idleTime))
+}
+
+pickRandom <- function(range) {
+  colsCount <- ncol(range)
+  random1 <- rnd(colsCount)
+  index <- floor(random1)
+  return(range[0, index])
+}
+
+pickNext <- function(range, rumber) {
+  index <- mod(number, ncol(range))
+  return(range[0, index])
+}
+
+pickByOrder <- function(orderedRange, number) {
+  return(
+    pickNext(orderedRange, number)
+  )
+}
+
+weightRoundRobin <- function(nodes, iteration) {
+  return(
+    pickByOrder(nodes, iteration)
+  )
+}
+
+roundRobin <- function(nodes, iteration) {
+  return(
+    pickNext(nodes, iteration)
+  )
+}
+
+randomBalancing <- function(conflicts) {
+  return(pickRandom(conficts))
+}
+
+resolveConflicts <- function(method, nodes, iteration) {
+  if (method == "random") {
+    return(randomBalancing(nodes))
+  }
+
+  if (method == "roundRobin") {
+    return(roundRobin(nodes))
+  }
+
+  if (method == "weightRoundRobin") {
+    return(weightRoundRobin(nodes, iteration))
+  }
+  
+  stop()
+}
+
+getNodesIds <- function(nodes) {
+  conflictedNodes <- c(0)
+  for (i in 1:len(nodes)) {
+    if (nodes[i] == 1) {
+      if (ncol(conflictedNodes) == 1 && conflictedNodes[0] == 0) {
+        conflictedNodes[0] <- i + 1
+        next()
+      }
+    conflictedNodes <- conflictedNodes + v(i + 1)
+    }
+  }
+  return(conflictedNodes)
+}
+
+getTransitionNumberWithResolving <- function(nodes, iteration, method) {
+  vectorLength <- len(nodes)
+  q <- 1
+  if (nodes == conflictedTransitions) {
+    conflicedNodes <- getNodesIds(nodes)
+    transitionNumber <- resolveConflict(method, conflicedNodes, iteration)
+    return(transitionNumber)
+  }
+  for (i in 1:vectorLength) {
+    if((q * nodes[i]) == 1) {
+      transitionNumber <- i + 1
+    }
+    if(nodes[i] > 0) {
+      q <- 0
+    }
+  }
+  return(transitionNumber)
+}
+
+getRandomTimeForLambda <- function(lambda) {
+  randomTime <- (-1/lambda) * ln(1 - rnd(1))
+  return(randomTime)
+}
+
+getTransitionTime <- function(currentTime, lambda) {
+  time <- currentTime + getRandomTimeForLambda(lambda)
+  return(time)
+}
+
+performTransition <- function(transition, processTime) {
+  lambda <- 0.1
+  transitionTime <- 0.0
+  if (transition == 1) {
+    lambda <- 0.1
+  }
+  if (transition == 2) {
+    lambda <- 0.2
+  }
+  if (transition == 3) {
+    lambda <- 0.3
+  }
+  if (transition == 4) {
+    lambda <- 0.4
+  }
+  if (transition == 5) {
+    lambda <- 0.5
+  }
+  if (transition == 6) {
+    lambda <- 0.6
+  }
+  if (transition == 7) {
+    lambda <- 0.7
+  }
+  if (transition == 8) {
+    lambda <- 0.8
+  }
+  if (transition == 9) {
+    lambda <- 0.9
+  }
+  transitionTime <- getRandomTimeForLambda(lambda)
+  return(processTime + transitionTime)
+}
+
+main <- function(inputFunc, outputFunc, M, number) {
+  commonFunc <- outputFunc - inputFunc
+  
+}
 
 
 
